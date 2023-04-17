3db7fd4ad8c6451f53d7ebac86f0c4f130997822
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-17 18:28:07 +0100

    Update and rename omni_TECNO-KG7h.mk to twrp_KG7h.mk

--- twrp_KG7h.mk
+++ twrp_KG7h.mk
@@ -10,16 +10,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from TECNO-KG7h device
-$(call inherit-product, device/tecno/TECNO-KG7h/device.mk)
+# Inherit from KG7h device
+$(call inherit-product, device/tecno/KG7h/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := TECNO-KG7h
-PRODUCT_NAME := omni_TECNO-KG7h
+PRODUCT_DEVICE := KG7h
+PRODUCT_NAME := twrp_KG7h
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KG7h
 PRODUCT_MANUFACTURER := tecno
